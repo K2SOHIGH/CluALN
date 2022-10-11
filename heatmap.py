7849d8f4bced8393388dcedb92bc09b4474fc62b
Maxime Millet <maxime.luc.millet@gmail.com>
2022-10-11 10:40:06 +0200

    .

---
+++ heatmap.py
@@ -0,0 +1,20 @@
+import seaborn as sns
+import pandas as pd
+import sys
+import os
+from matplotlib import pyplot as plt
+
+
+
+
+matrix = sys.argv[1]
+outmap = sys.argv[2]
+dirname = os.path.abspath(os.path.dirname(outmap))
+
+os.makedirs(dirname,exist_ok=True)
+df = pd.read_csv(matrix,sep="\t",header=0,index_col=0)
+fig = sns.clustermap(df,cmap="summer")
+
+
+
+plt.savefig(outmap)
